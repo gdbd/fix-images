4c77cbef0e57f43948d68f922dc26413822c5221
denis zatsarin <zds@live.ru>
2023-05-04 22:25:19 +0300

    feat: basic implementation for 'WP_*' files

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 import nconf from "nconf";
 import fs from "fs";
 import path from "path";
@@ -7,21 +5,85 @@ import "colors";
 
 nconf.argv().env();
 
+// where to find pictures
 var dir = nconf.get("dir");
+
+//which year modified to find
 var year = nconf.get("year");
 
+//write changes to file
+var write = nconf.get("write");
+
 console.log(dir);
+
+const dateFileNameExtractors = {
+  WP: /WP_(?<year>\d\d\d\d)(?<month>\d\d)(?<day>\d\d)_(?<hour>\d\d)_(?<minute>\d\d)/g,
+};
+
+const getExtractor = (file: string) => {
+  if (file.startsWith("WP_")) {
+    return dateFileNameExtractors.WP;
+  }
+  return undefined;
+};
+
+const extractDate = (file: string) => {
+  const extractor = getExtractor(file);
+
+  if (!extractor) {
+    return undefined;
+  }
+
+  const matches = file.matchAll(extractor);
+
+  const groups = matches.next().value.groups;
+
+  if (!groups) {
+    console.log("no groups".red);
+    return undefined;
+  }
+
+  const { year, month, day, hour, minute } = groups;
+
+  if (!year || !month || !day || !hour || !minute) {
+    console.log("not all date components found");
+    return undefined;
+  }
+
+  return new Date(
+    Number(year),
+    Number(month) - 1,
+    Number(day),
+    Number(hour),
+    Number(minute)
+  );
+};
 
 const files = fs.readdirSync(dir);
 
 for (const file of files) {
-  const stat = fs.statSync(path.join(dir, file));
+  const fullPath = path.join(dir, file);
+  const stat = fs.statSync(fullPath);
+
   if (stat.mtime.getFullYear() === year) {
-    console.log(file, stat.mtime.getFullYear());
-    if (file.startsWith("WP_")) {
-      console.log("WP".green);
-    } else {
-      console.log("unsupported".red);
+    console.log();
+    console.log(file.yellow);
+    const date = extractDate(file);
+
+    if (!date) {
+      console.log("cannot extract date".red);
+      continue;
+    }
+
+    console.log(`extracted ${date.toLocaleString()}`.green);
+
+    if (write) {
+      try {
+        fs.utimesSync(fullPath, date, date);
+        console.log("updated".green);
+      } catch (e) {
+        console.log("error".red, e);
+      }
     }
   }
 }
